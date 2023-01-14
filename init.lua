ecb328e1e3b0b252b73872aafab94c17eb705c9e
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2023-01-14 17:33:37 +0500

    teleporting to previous pos

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 core.register_on_protection_violation(function(pos, name)
     local player = core.get_player_by_name(name)
     if not player then return end
+    local pos = player:get_pos()
     local yaw = player:get_look_horizontal()
-    local dir = player:get_look_dir()
     local hp = player:get_hp()
+    player:set_pos(pos)
     player:set_look_horizontal(yaw+math.pi)
-    player:add_velocity({x=-dir.x*2,y=-dir.y*2,z=-dir.z*2})
     if hp > 0 and core.settings:get_bool("enable_damage") then
         core.after(0.1,function()
             player:set_hp(hp-1)
